0e80830d66d0cb1eca4136735bd9b9eb88bf54b7
Denis Sokolov <40318262+lorymore@users.noreply.github.com>
2019-09-17 16:16:54 +0500

    Add files via upload

---
+++ Liquid Image/app.js
@@ -0,0 +1,8 @@
+new hoverEffect({
+  parent: document.querySelector('.distortion'),
+  intensity: 0.2,
+  image1: './images/img1.jpeg',
+  image2: './images/img2.jpeg',
+  angle: Math.PI / 4,
+  displacementImage: './images/heightMap.png'
+});
